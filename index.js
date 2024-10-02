14ea60ccfa9d6ec0274a9afacfcd20c9a1a6765d
Culture-Coach <132301974+hbtucker@users.noreply.github.com>
2024-10-02 15:31:35 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-import createChart from './sunburst.js';
-
-// Fetch the data and create the chart
-fetch('./data/data.json')
-  .then(response => response.json())
-  .then(data => {
-    const chartElement = createChart(data);
-    document.getElementById('chart').appendChild(chartElement);
-  });
+export {default} from "./sunburst.js";
