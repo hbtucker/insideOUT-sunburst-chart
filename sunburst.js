1303be7755ce94d87a5ce566956ad91e746f5a75
Culture-Coach <132301974+hbtucker@users.noreply.github.com>
2024-10-04 18:35:11 +1300

    Update sunburst.js

--- sunburst.js
+++ sunburst.js
@@ -254,7 +254,7 @@ function _chart(d3, data) {
   function labelTransform(d) {
     const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
     const y = ((d.y0 + d.y1) / 2) * radius;
-    return `translate(${y},0)`;
+    return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
   }
 
   return svg.node();
