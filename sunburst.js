c804c80f98fdb0bdbc13c0314136f0c90ef75478
Culture-Coach <132301974+hbtucker@users.noreply.github.com>
2024-10-05 09:34:26 +1300

    Update sunburst.js

--- sunburst.js
+++ sunburst.js
@@ -246,11 +246,11 @@ label
     return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
   }
 
-function labelTransform(d) {
-  const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
-  const y = ((d.y0 + d.y1) / 2) * radius;
-  return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
-}
+  function labelTransform(d) {
+    const x = (((d.x0 + d.x1) / 2) * 180) / Math.PI;
+    const y = ((d.y0 + d.y1) / 2) * radius;
+    return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+  }
 
   // Dark mode toggle functionality
   function updateColors(isDarkMode) {
